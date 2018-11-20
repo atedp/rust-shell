fe67d3eeb9993cdf05039924dde91bd4c7b47a32
Ted Plummer <tplummer@fitbit.com>
2018-11-19 21:06:22 -0500

    gonna have another four roses and chill.  will maybe fix tomorrow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+// use std::env;
+use std::io::{stdin}; //, stdout, Write};
+// use std::path::Path;
+use std::process::{Child, Command, Stdio};
+
+fn main(){
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+
+    //read_line leaves a trailing newline, which trim removes
+    let command = input.trim();
+
+    Command::new(command)
+        .spawn()
+        .unwrap();
 }
